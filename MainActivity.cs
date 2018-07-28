67c91aca26ff758e83185824cb6f720bec3de5e5
Martin <40596348+MartiP33@users.noreply.github.com>
2018-07-28 17:35:22 +0200

    Add files

---
+++ MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace Time_lapse_app
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.menu_layout);
+
+            var photoModeButton = FindViewById<Button>(Resource.Id.photoModeButton);
+            var videoModeButton = FindViewById<Button>(Resource.Id.videoModeButton);
+            var aboutButton = FindViewById<Button>(Resource.Id.aboutButton);
+
+            photoModeButton.Click += PhotoModeButton_Click;
+
+
+
+        }
+
+        private void PhotoModeButton_Click(object sender, System.EventArgs e)
+        {
+            StartActivity(typeof(PhotoOptionsMenuActivity));
+        }
+    }
+}
---
+++ PhotoOptionsMenuActivity.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Time_lapse_app
+{
+    [Activity(Label = "OptionsMenuActivity")]
+    public class PhotoOptionsMenuActivity : Activity
+    {
+        int intervalTime = 10;
+        int shootingTime = 120;
+        
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.photo_options);
+
+            //Iskanje teh zadeu k jih lah prtisnes
+            var intervalChangeButton = FindViewById<Button>(Resource.Id.interval_change_button);
+            var shootingTimeChangeButton = FindViewById<Button>(Resource.Id.shooting_time_change_button);
+            var resolutionChangeButton = FindViewById<Button>(Resource.Id.resolution_change_button);
+
+            intervalChangeButton.Text = (intervalTime).ToString();
+            shootingTimeChangeButton.Text = (shootingTime).ToString();
+
+
+            intervalChangeButton.Click += delegate
+            {
+
+                LayoutInflater layoutInflaterAndroid = LayoutInflater.From(this);
+                View mView = layoutInflaterAndroid.Inflate(Resource.Layout.user_input_dialog_box, null);
+                Android.Support.V7.App.AlertDialog.Builder alerDialogBuilder = new Android.Support.V7.App.AlertDialog.Builder(this);
+                alerDialogBuilder.SetView(mView);
+
+                var userIntervalTime = mView.FindViewById<EditText>(Resource.Id.dialog_edit_text);
+                
+                alerDialogBuilder.SetCancelable(false)
+                .SetPositiveButton("Ok", delegate
+                {
+                    intervalTime = Convert.ToInt32(userIntervalTime.Text);
+                    Toast.MakeText(this, "Interval time set to " + intervalTime + " seconds", ToastLength.Short).Show();
+                    intervalChangeButton.Text = (intervalTime).ToString();
+                })
+                .SetNegativeButton("Cancel", delegate
+                {
+                    alerDialogBuilder.Dispose();
+                });
+
+                Android.Support.V7.App.AlertDialog alertDialogAndroid = alerDialogBuilder.Create();
+                alertDialogAndroid.Show();
+
+                
+            };
+
+            shootingTimeChangeButton.Click += delegate
+            {
+                LayoutInflater layoutInflaterAndroid = LayoutInflater.From(this);
+                View mView = layoutInflaterAndroid.Inflate(Resource.Layout.user_input_dialog_box, null);
+                Android.Support.V7.App.AlertDialog.Builder alerDialogBuilder = new Android.Support.V7.App.AlertDialog.Builder(this);
+                alerDialogBuilder.SetView(mView);
+
+                var userShootingTime = mView.FindViewById<EditText>(Resource.Id.dialog_edit_text);
+
+                alerDialogBuilder.SetCancelable(false)
+                .SetPositiveButton("Ok", delegate
+                {
+                    shootingTime = Convert.ToInt32(userShootingTime.Text);
+                    Toast.MakeText(this, "Shooting time set to " + shootingTime + " minutes", ToastLength.Short).Show();
+                    intervalChangeButton.Text = (shootingTime).ToString();
+                })
+                .SetNegativeButton("Cancel", delegate
+                {
+                    alerDialogBuilder.Dispose();
+                });
+
+                Android.Support.V7.App.AlertDialog alertDialogAndroid = alerDialogBuilder.Create();
+                alertDialogAndroid.Show();
+            };
+
+            
+
+        }
+    }
+}
